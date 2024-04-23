46a5d4da0589b5aa1e767f4c457785fb6b6ca6ca
jacob-kuch <jacob.kuch@trojans.dsu.edu>
2024-04-23 17:50:03 -0500

    changed startup to open correct form (AdvancedCalc)

--- CSC455_ProjectCalculator/Program.cs
+++ CSC455_ProjectCalculator/Program.cs
@@ -16,7 +16,7 @@ namespace CSC455_ProjectCalculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AdvancedCalc());
         }
     }
 }
