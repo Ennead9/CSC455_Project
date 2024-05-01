8a0230d307a323430a8c6a8b60caf8826326b08f
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2024-04-30 23:10:46 -0500

    Change input to add numbers to list

--- CSC455_ProjectCalculator/AdvancedCalc.cs
+++ CSC455_ProjectCalculator/AdvancedCalc.cs
@@ -30,10 +30,8 @@ namespace CSC455_ProjectCalculator
         #region Circle Perimeter
         private void circlePerim_Click(object sender, EventArgs e)
         {
-
             selectedCalculation = "circlePerim";
             label1.Text = "Enter positive value for diameter d";
-            
         }
         private double calcCirclePerim(double d)
         {
@@ -41,12 +39,21 @@ namespace CSC455_ProjectCalculator
         }
         #endregion
 
-        #region
+        #region Triangle Perimeter
+
         private void trianglePerim_Click(object sender, EventArgs e)
         {
-
+            selectedCalculation = "trianglePerim";
+            label1.Text = "Enter positive vals for a,b,c separated by a space";
+        }
+        private double calcTrianglePerim(double a, double b, double c)
+        {
+            return a + b + c;
         }
 
+        #endregion
+
+        #region
         private void rectPerim_Click(object sender, EventArgs e)
         {
 
@@ -87,32 +94,66 @@ namespace CSC455_ProjectCalculator
         }
         #endregion
 
+        #region Clear & Calculate Buttons
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+
+            // Get text from textBox1
+            string inputText = textBox1.Text.Trim();
+
+            // Exit early if no input
+            if (string.IsNullOrEmpty(inputText))
+            {
+                label1.Text = "Error: No input";
+                return;
+            }
+
+            // Split numbers using space as delimiter
+            string[] inputs = inputText.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            // Create list for parsed doubles and add them to list
+            List<double> numbers = new List<double>();
+
+            foreach(string input in inputs)
+            {
+                double number;
+
+                // Add if number is a valid double
+                if (double .TryParse(input, out number))
+                { 
+                    numbers.Add(number); 
+                }
+                else 
+                {
+                    label1.Text = "Invalid input. Enter numbers separated by spaces";
+                }
+            }
+
             switch (selectedCalculation)
             {
                 case "circlePerim":
 
-                    double d;
-                    if (double.TryParse(textBox1.Text, out d) && d > 0)
+                    double d = numbers[0];
+
+                    if (d > 0 && numbers.Count == 1)
                     {
-                            double perimeter = calcCirclePerim(d);
-                            textBox1.Text = perimeter.ToString("N2"); // Format to 2 decimal places
+                        double perimeter = calcCirclePerim(d);
+                        textBox1.Text = perimeter.ToString("N2"); // Format to 2 decimal places
                     }
                     else
                     {
-                            MessageBox.Show("Please enter a single, positive number for diameter");
-                            textBox1.Text = ""; // Clear textbox for new input
+                        MessageBox.Show("Please enter a single, positive number for diameter");
+                        textBox1.Text = ""; // Clear textbox for new input
                     }
 
                     break;
             }
         }
-
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = "";
-        }
+        #endregion
 
         private void AdvancedCalc_Load(object sender, EventArgs e)
         {
