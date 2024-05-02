0a5b66a0c7f21fec57b3c30792e35437cd349b59
Isaac_R <isaac.rosdail@trojans.dsu.edu>
2024-05-02 14:57:29 -0500

    Both got the unit testing up and running, made a dummy test to ensure it works as expected

---
+++ CSC455_ProjectCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSC455_ProjectCalculator;
+using System;
+
+namespace CSC455_ProjectCalculator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var calculator = new CSC455_ProjectCalculator.AdvancedCalc();
+            Assert.IsNotNull(calculator);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
